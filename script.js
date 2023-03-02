559fe2678806ad249633c90a246d48cc642517cc
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-03-02 19:06:06 +0100

    holis

---
+++ script.js
@@ -0,0 +1,383 @@
+//desing page
+let canvas = document.createElement('canvas')
+let width = canvas.width = window.innerWidth * 0.75;
+let height = canvas.height = window.innerHeight * 0.75
+document.body.appendChild(canvas);
+let gl = canvas.getContext('webgl');
+
+let mouse = {x:0,y:0};
+
+var numMetaballs = 30;
+var metaballs = [];
+
+for (var i = 0; i < numMetaballs; i++) {
+  var radius = Math.random() * 60 + 10;
+  metaballs.push({
+    x: Math.random() * (width - 2 * radius) + radius,
+    y: Math.random() * (height - 2 * radius) + radius,
+    vx: (Math.random() - 0.5) * 3,
+    vy: (Math.random() - 0.5) * 3,
+    r: radius * 0.75
+  });
+}
+
+var vertexShaderSrc = `
+attribute vec2 position;
+
+void main() {
+// position specifies only x and y.
+// We set z to be 0.0, and w to be 1.0
+gl_Position = vec4(position, 0.0, 1.0);
+}
+`;
+
+var fragmentShaderSrc = `
+precision highp float;
+
+const float WIDTH = ` + (width >> 0) + `.0;
+const float HEIGHT = ` + (height >> 0) + `.0;
+
+uniform vec3 metaballs[` + numMetaballs + `];
+
+void main(){
+float x = gl_FragCoord.x;
+float y = gl_FragCoord.y;
+
+float sum = 0.0;
+for (int i = 0; i < ` + numMetaballs + `; i++) {
+vec3 metaball = metaballs[i];
+float dx = metaball.x - x;
+float dy = metaball.y - y;
+float radius = metaball.z;
+
+sum += (radius * radius) / (dx * dx + dy * dy);
+}
+
+if (sum >= 0.99) {
+gl_FragColor = vec4(mix(vec3(x / WIDTH, y / HEIGHT, 1.0), vec3(0, 0, 0), max(0.0, 1.0 - (sum - 0.99) * 100.0)), 1.0);
+return;
+}
+
+gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}
+
+`;
+
+var vertexShader = compileShader(vertexShaderSrc, gl.VERTEX_SHADER);
+var fragmentShader = compileShader(fragmentShaderSrc, gl.FRAGMENT_SHADER);
+
+var program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+gl.useProgram(program);
+
+var vertexData = new Float32Array([
+  -1.0,  1.0, // top left
+  -1.0, -1.0, // bottom left
+  1.0,  1.0, // top right
+  1.0, -1.0, // bottom right
+]);
+var vertexDataBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+var positionHandle = getAttribLocation(program, 'position');
+gl.enableVertexAttribArray(positionHandle);
+gl.vertexAttribPointer(positionHandle,
+                       2, // position is a vec2
+                       gl.FLOAT, // each component is a float
+                       gl.FALSE, // don't normalize values
+                       2 * 4, // two 4 byte float components per vertex
+                       0 // offset into each span of vertex data
+                      );
+
+var metaballsHandle = getUniformLocation(program, 'metaballs');
+
+loop();
+function loop() {
+  for (var i = 0; i < numMetaballs; i++) {
+    var metaball = metaballs[i];
+    metaball.x += metaball.vx;
+    metaball.y += metaball.vy;
+
+    if (metaball.x < metaball.r || metaball.x > width - metaball.r) metaball.vx *= -1;
+    if (metaball.y < metaball.r || metaball.y > height - metaball.r) metaball.vy *= -1;
+  }
+
+  var dataToSendToGPU = new Float32Array(3 * numMetaballs);
+  for (var i = 0; i < numMetaballs; i++) {
+    var baseIndex = 3 * i;
+    var mb = metaballs[i];
+    dataToSendToGPU[baseIndex + 0] = mb.x;
+    dataToSendToGPU[baseIndex + 1] = mb.y;
+    dataToSendToGPU[baseIndex + 2] = mb.r;
+  }
+  gl.uniform3fv(metaballsHandle, dataToSendToGPU);
+  
+  //Draw
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  requestAnimationFrame(loop);
+}
+
+function compileShader(shaderSource, shaderType) {
+  var shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderSource);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw "Shader compile failed with: " + gl.getShaderInfoLog(shader);
+  }
+
+  return shader;
+}
+
+function getUniformLocation(program, name) {
+  var uniformLocation = gl.getUniformLocation(program, name);
+  if (uniformLocation === -1) {
+    throw 'Can not find uniform ' + name + '.';
+  }
+  return uniformLocation;
+}
+
+function getAttribLocation(program, name) {
+  var attributeLocation = gl.getAttribLocation(program, name);
+  if (attributeLocation === -1) {
+    throw 'Can not find attribute ' + name + '.';
+  }
+  return attributeLocation;
+}
+
+canvas.onmousemove = function(e) {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
+
+
+
+
+
+
+
+
+
+const playPauseBtn = document.querySelector(".play-pause-btn");
+const theaterBtn = document.querySelector(".theater-btn");
+const fullScreenBtn = document.querySelector(".full-screen-btn");
+const miniPlayerBtn = document.querySelector(".mini-player-btn");
+const muteBtn = document.querySelector(".mute-btn");
+const captionsBtn = document.querySelector(".captions-btn");
+const speedBtn = document.querySelector(".speed-btn");
+const currentTimeElem = document.querySelector(".current-time");
+const totalTimeElem = document.querySelector(".total-time");
+const thumbnailImg = document.querySelector(".thumbnail-img");
+const volumeSlider = document.querySelector(".volume-slider");
+const videoContainer = document.querySelector(".video-container");
+const timelineContainer = document.querySelector(".timeline-container");
+const video = document.querySelector("video");
+const mute = document.getElementById("mute");
+
+
+mute.addEventListener("click",function(){
+  if(video.muted){
+    video.muted = false;
+    mute.innerHTML="mute";
+  }else{
+    video.muted = true;
+    mute.innerHTML='unmuted'
+  }
+})
+
+
+document.addEventListener("keydown", (e) => {
+  const tagName = document.activeElement.tagName.toLowerCase();
+
+  if (tagName === "input") return;
+
+  switch (e.key.toLowerCase()) {
+    case " ":
+      if (tagName === "button") return;
+    case "k":
+      togglePlay();
+      break;
+    case "f":
+      toggleFullScreenMode();
+      break;
+    case "t":
+      toggleTheaterMode();
+      break;
+    case "i":
+      toggleMiniPlayerMode();
+      break;
+    case "m":
+      toggleMute();
+      break;
+    case "arrowleft":
+    case "j":
+      skip(-5);
+      break;
+    case "arrowright":
+    case "l":
+      skip(5);
+      break;
+    case "c":
+      toggleCaptions();
+      break;
+  }
+});
+
+// Timeline
+timelineContainer.addEventListener("mousemove", handleTimelineUpdate);
+timelineContainer.addEventListener("mousedown", toggleScrubbing);
+document.addEventListener("mouseup", (e) => {
+  if (isScrubbing) toggleScrubbing(e);
+});
+document.addEventListener("mousemove", (e) => {
+  if (isScrubbing) handleTimelineUpdate(e);
+});
+
+let isScrubbing = false;
+let wasPaused;
+function toggleScrubbing(e) {
+  const rect = timelineContainer.getBoundingClientRect();
+  const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+  isScrubbing = (e.buttons & 1) === 1;
+  videoContainer.classList.toggle("scrubbing", isScrubbing);
+  if (isScrubbing) {
+    wasPaused = video.paused;
+    video.pause();
+  } else {
+    video.currentTime = percent * video.duration;
+    if (!wasPaused) video.play();
+  }
+
+  handleTimelineUpdate(e);
+}
+
+function handleTimelineUpdate(e) {
+  const rect = timelineContainer.getBoundingClientRect();
+  const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
+
+  1, Math.floor((percent * video.duration) / 10);
+}
+
+// Playback Speed
+speedBtn.addEventListener("click", changePlaybackSpeed);
+
+function changePlaybackSpeed() {
+  let newPlaybackRate = video.playbackRate + 0.25;
+  if (newPlaybackRate > 2) newPlaybackRate = 0.25;
+  video.playbackRate = newPlaybackRate;
+  speedBtn.textContent = `${newPlaybackRate}x`;
+}
+
+// Duration
+video.addEventListener("loadeddata", () => {
+  totalTimeElem.textContent = formatDuration(video.duration);
+});
+
+video.addEventListener("timeupdate", () => {
+  currentTimeElem.textContent = formatDuration(video.currentTime);
+  const percent = video.currentTime / video.duration;
+  timelineContainer.style.setProperty("--progress-position", percent);
+});
+
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2,
+});
+function formatDuration(time) {
+  const seconds = Math.floor(time % 60);
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600);
+  if (hours === 0) {
+    return `${minutes}:${leadingZeroFormatter.format(seconds)}`;
+  } else {
+    return `${hours}:${leadingZeroFormatter.format(
+      minutes
+    )}:${leadingZeroFormatter.format(seconds)}`;
+  }
+}
+
+function skip(duration) {
+  video.currentTime += duration;
+}
+
+// Volume
+muteBtn.addEventListener("click", toggleMute);
+volumeSlider.addEventListener("input", (e) => {
+  video.volume = e.target.value;
+  video.muted = e.target.value === 0;
+});
+
+function toggleMute() {
+  video.muted = !video.muted;
+}
+
+video.addEventListener("volumechange", () => {
+  volumeSlider.value = video.volume;
+  let volumeLevel;
+  if (video.muted || video.volume === 0) {
+    volumeSlider.value = 0;
+    volumeLevel = "muted";
+  } else if (video.volume >= 0.5) {
+    volumeLevel = "high";
+  } else {
+    volumeLevel = "low";
+  }
+
+  videoContainer.dataset.volumeLevel = volumeLevel;
+});
+
+// View Modes
+theaterBtn.addEventListener("click", toggleTheaterMode);
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+
+function toggleTheaterMode() {
+  videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+function toggleMiniPlayerMode() {
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture();
+  } else {
+    video.requestPictureInPicture();
+  }
+}
+
+document.addEventListener("fullscreenchange", () => {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
+
+video.addEventListener("enterpictureinpicture", () => {
+  videoContainer.classList.add("mini-player");
+});
+
+video.addEventListener("leavepictureinpicture", () => {
+  videoContainer.classList.remove("mini-player");
+});
+
+// Play/Pause
+playPauseBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+video.addEventListener("play", () => {
+  videoContainer.classList.remove("paused");
+});
+
+video.addEventListener("pause", () => {
+  videoContainer.classList.add("paused");
+});
